72a199cf63a8b5a425df93b227f438993fdd490e
OpenCv_python_detect_lua <khgammingcraft@gmail.com>
2022-04-14 09:51:52 +0700

    update facereco 1.5/2

--- PictureData/__trainning__.py
+++ PictureData/__trainning__.py
@@ -5,21 +5,20 @@ import os
 # run this first
 # pip install opencv-contrib-python
 people = []
+features = []
+labels = []
 dir = "face_data"
 haar_cascade = cv.CascadeClassifier("./face_detect.xml")
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 
-features = []
-labels = []
+
 
 # ================================ Save Face Object (LABEL) ================================
 
-for i in os.listdir(r'./face_data'):
-    people.append(i)
+def train_running():
 
-# ================================ Face Trainning ================================
-def trainning_face():
-
+    for i in os.listdir(r'face_data'):
+        people.append(i)
     #  Read Each Of Picture In Dir
     for person in people:
         path = os.path.join(dir, person)
@@ -32,7 +31,6 @@ def trainning_face():
             img = cv.imread(img_path)
             gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-
             faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
             print(len(faces_rect))
@@ -44,13 +42,13 @@ def trainning_face():
                 features.append(faces_roi)
                 labels.append(label)
 
-trainning_face()
+
+# ================================ Save_Face_Reco ================================
+
+train_running()
 
 features = np.array(features, dtype="object")
 labels = np.array(labels)
 
-# ================================ Save_Face_Reco ================================
-
 face_recognizer.train(features, labels)
 face_recognizer.save("face_trained.yml")
-
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import *
 from markupsafe import escape
 import cv2
+import os
 
+haar_cascade = cv2.CascadeClassifier('PictureData/face_detect.xml')
+
+people = []
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('PictureData/face_trained.yml')
+
+
+# cv2.imshow("Ben", img)
+for i in os.listdir(r'PictureData/face_data'):
+    people.append(i)
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -11,22 +25,30 @@ def hello_world():  # put application's code here
 
 
 def gen():
+
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
 
+
+
         if not ret:
             print("Error: failed to capture image")
             break
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        haar_cascade = cv2.CascadeClassifier('face_detect.xml')
+        # ================================ Face_trained reading here ================================
 
-        faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+        # Detect the face in the image
+        faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
 
-        # print(f'Number of face found = {len(faces_rect)}')
         for (x, y, w, h) in faces_rect:
+            faces_roi = gray[y:y + h, x:x + h]
+
+            label, confidence = face_recognizer.predict(faces_roi)
+            print(f'Label = {people[label]} with a confidence of {confidence}')
+
+            cv2.putText(frame, str(people[label]), (20, 20), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), thickness=2)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
 
         cv2.imwrite('test_pic.jpg', frame)
@@ -42,7 +64,7 @@ def video_feed():
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return "nothing 404", 404
 
 
 if __name__ == '__main__':
